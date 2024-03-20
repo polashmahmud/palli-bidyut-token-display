3ec10f2bc4f54cc7f06572f8761494a067fc66a5
Polash Mahmud <polashmahmud@gmail.com>
2024-03-20 21:27:09 +0600

    fix padding

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="max-w-md mx-auto py-6">
+  <div class="max-w-md mx-auto py-6 p-6">
     <RouterView />
   </div>
 </template>
