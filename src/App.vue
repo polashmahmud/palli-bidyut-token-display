f3e47483a43b2fb962c0665582bac2d9e55665ab
royhridoy <royhridoy1428@gmail.com>
2024-03-21 19:08:19 +0600

    UI Updated

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="max-w-md mx-auto py-6 p-6">
-    <RouterView />
+  <div class="flex items-center justify-center min-h-screen bg-dark-blue">
+    <div class="max-w-md sm:min-w-[576px] mx-5 sm:mx-auto rounded-xl overflow-hidden shadow-current shadow-md bg-dark-blue-light">
+      <RouterView />
+    </div>
   </div>
 </template>
