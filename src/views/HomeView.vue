06253ea143f4c726cff633ab910720d7417302c2
Polash Mahmud <polashmahmud@gmail.com>
2024-03-21 22:32:54 +0600

    change design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,14 +43,28 @@ const reset = () => {
 
 <template>
   <div>
+    <div v-if="!newToken.length" class="flex p-4 mb-4 text-sm rounded-lg bg-gray-800 text-blue-400"
+         role="alert">
+      <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+           fill="currentColor" viewBox="0 0 20 20">
+        <path
+            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+      </svg>
+      <span class="sr-only">ব্যবহারবিধি:</span>
+      <div>
+        <span class="font-medium">ব্যবহারবিধি:</span>
+        <ul class="mt-1.5 list-disc list-inside">
+          <li>আপনার মিটারের মেসেজটি কপি করে নিচের বক্সে পেষ্ট করুন।</li>
+          <li>Get Token বাটনে চাপ দিন।</li>
+          <li>Token entry দিন এবং Next Token বাটনে ক্লিক করে পরবর্তী Token দেখুন।</li>
+        </ul>
+      </div>
+    </div>
+
     <div class="pb-4 mb-6 text-center">
-      <h1 class="px-2 py-5 mb-4 text-[20px] xs:text-[16px] sm:text-[20px] font-medium sm:text-3xl bg-sky-blue-light text-dark-blue-light-2 font-Roboto">Palli Bidyut <span class="px-4 py-1 rounded-lg bg-dark-blue-light-2 text-sky-blue-light">Token Display</span></h1>
-	  <ul class="flex flex-col px-2 space-y-2 text-left">
-		<li class="px-4 text-sm font-bold sm:text-base text-sky-blue-light/80">ব্যবহারবিধি:</li>
-		<li class="px-4 text-sm sm:text-base text-sky-blue-light/80"><span class="underline underline-offset-4"> ধাপ ১:</span> আপনার মিটারের মেসেজটি কপি করে নিচের বক্সে পেষ্ট করুন।</li>
-		<li class="px-4 text-sm sm:text-base text-sky-blue-light/80"><span class="underline underline-offset-4"> ধাপ ২:</span> Get Token বাটনে চাপ দিন।</li>
-		<li class="px-4 text-sm sm:text-base text-sky-blue-light/80"><span class="underline underline-offset-4"> ধাপ ৩:</span> একটি একটি করে Token entry দিন এবং Next Token বাটনে ক্লিক করে পরবর্তী Token দেখুন।</li>
-	  </ul>
+      <h1 class="px-2 py-5 mb-4 text-[20px] xs:text-[16px] sm:text-[20px] font-medium sm:text-3xl bg-sky-blue-light text-dark-blue-light-2 font-Roboto">
+        Palli Bidyut <span class="px-4 py-1 rounded-lg bg-dark-blue-light-2 text-sky-blue-light">Token Display</span>
+      </h1>
     </div>
     <div v-if="newToken.length" class="px-6 pb-9">
       <div class="text-sky-blue-light/40 text-end">
@@ -59,7 +73,8 @@ const reset = () => {
 
       <div class="space-y-6">
         <div>
-          <div class="px-2 py-4 text-xl font-semibold text-center border rounded-lg sm:text-3xl border-sky-blue-light/50 text-sky-blue-light">
+          <div
+              class="px-2 py-4 text-xl font-semibold text-center border rounded-lg sm:text-3xl border-sky-blue-light/50 text-sky-blue-light">
             {{ newToken[tokenIndex] }}
           </div>
         </div>
@@ -73,7 +88,10 @@ const reset = () => {
         </div>
 
         <div class="text-center">
-          <button class="px-4 py-2 transition-all duration-300 bg-transparent border rounded-lg hover:text-white text-sky-blue-light border-sky-blue-light/20 hover:bg-sky-blue-light/10 hover:shadow-sm shadow-current" @click="reset">New Token</button>
+          <button
+              class="px-4 py-2 transition-all duration-300 bg-transparent border rounded-lg hover:text-white text-sky-blue-light border-sky-blue-light/20 hover:bg-sky-blue-light/10 hover:shadow-sm shadow-current"
+              @click="reset">New Token
+          </button>
         </div>
       </div>
     </div>
@@ -87,15 +105,21 @@ const reset = () => {
           v-model="token"
       />
       <div class="text-center">
-        <button class="px-4 py-2 text-white transition-all duration-300 rounded-lg bg-sky-blue-light/20 hover:bg-sky-blue-light/30 hover:shadow-sm shadow-current" @click="generate">Get Token</button>
+        <button
+            class="px-4 py-2 text-white transition-all duration-300 rounded-lg bg-sky-blue-light/20 hover:bg-sky-blue-light/30 hover:shadow-sm shadow-current"
+            @click="generate">Get Token
+        </button>
       </div>
     </div>
 
-    <div class="flex flex-col items-center py-2 space-y-1 text-sm sm:space-y-0 sm:space-x-3 sm:items-center sm:justify-center sm:flex-row bg-dark-blue-light-2 text-sky-blue-light/70">
-      <span >Developed by:</span>
-      <a href="https://github.com/polashmahmud" target="_blank" class="hover:underline hover:text-sky-blue-light/80 underline-offset-4">Polash Mahmud</a>
+    <div
+        class="flex flex-col items-center py-2 space-y-1 text-sm sm:space-y-0 sm:space-x-3 sm:items-center sm:justify-center sm:flex-row bg-dark-blue-light-2 text-sky-blue-light/70">
+      <span>Developed by:</span>
+      <a href="https://github.com/polashmahmud" target="_blank"
+         class="hover:underline hover:text-sky-blue-light/80 underline-offset-4">Polash Mahmud</a>
       <span>and</span>
-      <a href="https://github.com/royhridoy" target="_blank" class="hover:underline hover:text-sky-blue-light/80 underline-offset-4"> Hridoy Roy </a>
+      <a href="https://github.com/royhridoy" target="_blank"
+         class="hover:underline hover:text-sky-blue-light/80 underline-offset-4"> Hridoy Roy </a>
     </div>
 
   </div>
